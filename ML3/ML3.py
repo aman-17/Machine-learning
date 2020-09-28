6ffb5eb44eb69f0e301b033d0a79ff5be38cce74
Aman Rangapur <44740048+aman-17@users.noreply.github.com>
2020-09-28 15:41:38 +0530

    Update ML3.py

--- ML3/ML3.py
+++ ML3/ML3.py
@@ -1,1 +1,57 @@
+#Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
+#Load dataset
+url = "https://raw.githubusercontent.com/callxpert/datasets/master/Loan-applicant-details.csv"
+names = ['Loan_ID','Gender','Married','Dependents','Education','Self_Employed','ApplicantIncome','CoapplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History','Property_Area','Loan_Status']
+dataset = pd.read_csv(url, names=names)
+print(dataset.head(20))
+
+#sklearn requires all inputs to be numeric, we should convert all our categorical variables into numeric by encoding the categories.
+from sklearn.preprocessing import LabelEncoder
+var_mod = ['Gender','Married','Dependents','Education','Self_Employed','Property_Area','Loan_Status']
+le = LabelEncoder()
+for i in var_mod:
+    dataset[i] = le.fit_transform(dataset[i])
+'''    
+As we have seen already, In Machine learning we have two kinds of datasets
+
+    Training dataset - used to train our model
+    Testing dataset - used to test if our model is making accurate predictions
+    
+'''
+array = dataset.values
+X = array[:,6:11]
+Y = array[:,12]
+x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y, test_size=0.2, random_state=7)
+
+#Logistic Regression : Logistic Regression is a classification algorithm. It is used to predict a binary outcome (1 / 0, Yes / No, True / False) given a set of independent variables. 
+#To represent binary / categorical outcome, we use dummy variables
+model = LogisticRegression()
+model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+print(accuracy_score(y_test, predictions))
+
+#Decision tree : Decision tree is a type of supervised learning algorithm (having a pre-defined target variable) that is mostly used in classification problems.
+#It works for both categorical and continuous input and output variables.
+model = DecisionTreeClassifier()
+model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+print(accuracy_score(y_test, predictions))
+
+#Random forest : Random forests or random decision forests are an ensemble learning method for classification, regression and other tasks,
+#that operate by constructing a multitude of decision trees at training time and outputting the class that is the mode of the classes (classification) or mean prediction (regression) of the individual trees
+model = RandomForestClassifier(n_estimators=100)
+model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+print(accuracy_score(y_test, predictions))
